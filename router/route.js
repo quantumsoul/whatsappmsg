c57ae856f38b70b9439022668f1212187932e3e9
quantumsoul <prakhar24012000@gmail.com>
2022-07-12 16:32:00 +0530

    init commit

--- router/route.js
+++ router/route.js
@@ -4,13 +4,13 @@ router.post("/send/pdf",async(req,res)=>{
     try {
         const accountSid = req.body.accountSid
         const authToken = req.body.authToken
-        // const url = req.body.url
+        const url = req.body.url
         var number = req.body.number
         const client = require('twilio')(accountSid, authToken)
         const Num = "whatsapp:" + "+91" + `${number}`
         client.messages
         .create({
-            // mediaUrl: [`${url}`],
+            mediaUrl: [`${url}`],
             body: 'Hello',
             from: 'whatsapp:+14155238886',
             to: `${Num}`
