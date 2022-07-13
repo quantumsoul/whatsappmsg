84566e0d553d01de5fbad78fc6aeff5942a0fc66
quantumsoul <prakhar24012000@gmail.com>
2022-07-13 20:02:29 +0530

    env update

--- router/route.js
+++ router/route.js
@@ -2,8 +2,8 @@ const express = require("express")
 const router = express.Router()
 router.post("/send/pdf",async(req,res)=>{
     try {
-        const accountSid = req.body.accountSid
-        const authToken = req.body.authToken
+        const accountSid = "AC913d0204e11592428f9fbc0ee87664fe"
+        const authToken = "e1de3bc1a936bb353eb1d36de27d8324"
         const url = req.body.url
         var number = req.body.number
         const client = require('twilio')(accountSid, authToken)
